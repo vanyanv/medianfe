805d5c24efb5d0af8373390a9de1f2c76be72ce1
Vardan <vardan.vanyan@gmail.com>
2024-09-03 11:24:23 -0700

    made function modal

--- app/componenets/Post.tsx
+++ app/componenets/Post.tsx
@@ -11,8 +11,34 @@ import React from 'react';
   updatedAt: '2024-08-31T03:33:20.377Z';
 }
 
-export default function Post({ post }) {
+type PostTypes = {
+  post: {
+    authorId: string | null;
+    body: string;
+    createdAt: string;
+    description: string;
+    id: number;
+    published: boolean;
+    title: string;
+    updatedAt: string;
+  };
+  isLoading: boolean;
+};
+
+export default function Post({ post, isLoading }: PostTypes) {
   const { authorId, body, createdAt, description, id, title } = post;
+
+  const randomNumber = Math.floor(Math.random() * (30 - 1 + 1)) + 1;
+  const ranadomAvatar = `https://robohash.org/Carvana${randomNumber}?set=set4`;
+
+  const formattedDate = new Date(createdAt).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+
+  if (isLoading) return <div>Loading..</div>;
+
   return (
     <>
       <article
@@ -20,8 +46,8 @@ export default function Post({ post }) {
         className='flex max-w-xl flex-col items-start justify-between'
       >
         <div className='flex items-center gap-x-4 text-xs'>
-          <time dateTime={createdAt} className='text-gray-500'>
-            {createdAt}
+          <time dateTime={formattedDate} className='text-gray-500'>
+            {formattedDate}
           </time>
           <a
             // href={post.category.href}
@@ -34,27 +60,27 @@ export default function Post({ post }) {
           <h3 className='mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600'>
             <a>
               <span className='absolute inset-0' />
-              {title}
+              {description}
             </a>
           </h3>
           <p className='mt-5 line-clamp-3 text-sm leading-6 text-gray-600'>
-            {description}
+            {body}
           </p>
         </div>
         <div className='relative mt-8 flex items-center gap-x-4'>
           <img
             alt=''
-            // src={post.author.imageUrl}
+            src={ranadomAvatar}
             className='h-10 w-10 rounded-full bg-gray-50'
           />
           <div className='text-sm leading-6'>
             <p className='font-semibold text-gray-900'>
-              <a href={authorId}>
+              <a>
                 <span className='absolute inset-0' />
                 {authorId}
               </a>
             </p>
-            {/* <p className='text-gray-600'>{post.author.role}</p> */}
+            <p className='text-gray-600'></p>
           </div>
         </div>
       </article>
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,47 @@
 'use client';
+import { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import getArticles from './utils/getArticles';
 import Post from './componenets/Post';
+import Navbar from './componenets/Navbar';
+import LoginModal from './componenets/LoginModal';
+
+type Post = {
+  authorId: string | null;
+  body: string;
+  createdAt: string;
+  description: string;
+  id: number;
+  published: boolean;
+  title: string;
+  updatedAt: string;
+};
+
 export default function Home() {
+  const [open, setOpen] = useState(false);
   const { data, isLoading, isError } = useQuery({
     queryFn: getArticles,
     queryKey: ['articles'],
   });
 
-  console.log(data);
   return (
-    <div className='bg-white py-24 sm:py-32'>
-      <div className='mx-auto max-w-7xl px-6 lg:px-8'>
-        <div className='mx-auto max-w-2xl'>
-          <h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
-            From the blog
-          </h2>
-          <p className='mt-2 text-lg leading-8 text-gray-600'>
-            Learn how to grow your business with our expert advice.
-          </p>
-          <div className='mt-10 space-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16'>
-            {data?.map((post) => (
-              <Post key={post.id} post={post} />
-            ))}
+    <div className='mx-auto max-w-7xl sm:px-6 lg:px-8 bg-white'>
+      <Navbar setOpen={setOpen} />
+      <div className='mx-auto max-w-3xl'>
+        <div className='mx-auto max-w-7xl px-6 lg:px-8'>
+          <div className='mx-auto max-w-2xl '>
+            <h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl text-center'>
+              Latest Articles
+            </h2>
+            <p className='mt-2 text-lg leading-8 text-gray-600 text-center'>
+              Explore our collection of insightful articles.
+            </p>
+            {open && <LoginModal open={open} setOpen={setOpen} />}
+            <div className='mt-10 space-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 bg-white'>
+              {data?.map((post: Post) => (
+                <Post key={data.id} post={post} isLoading={isLoading} />
+              ))}
+            </div>
           </div>
         </div>
       </div>
